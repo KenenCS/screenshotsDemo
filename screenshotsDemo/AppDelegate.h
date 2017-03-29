83e194784f98f6bd57f5ed31d26f7042dfc76cc0
KenenCS <KenenCS@163.com>
2017-03-29 09:50:42 +0800

    Initial Commit

---
+++ screenshotsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  screenshotsDemo
+//
+//  Created by kenen on 2017/3/29.
+//  Copyright © 2017年 kenen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
